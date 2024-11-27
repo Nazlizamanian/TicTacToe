042e45483b9328a2861228db37649ff4272d3ae0
Nazli Zamanian <nazlizamanian@gmail.com>
2024-11-27 15:11:14 +0100

    CheckWin and GameBoard

--- app/src/main/java/com/example/tictactoenazli/TicTacToe.kt
+++ app/src/main/java/com/example/tictactoenazli/TicTacToe.kt
@@ -3,7 +3,11 @@ package com.example.tictactoenazli
 import androidx.compose.material3.ListItem
 import android.content.Context
 import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,8 +15,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
@@ -27,15 +36,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.tictactoenazli.ui.theme.BabyPink
 import kotlinx.coroutines.flow.asStateFlow
-
+import java.time.format.TextStyle
 
 
 @Composable
@@ -168,7 +184,7 @@ fun LobbyScreen(navController: NavController, model: GameModel) {
                                             }
                                             .addOnFailureListener {
                                                 Log.e(
-                                                    "RobinError",
+                                                    "Error",
                                                     "Error updating game: $gameId"
                                                 )
                                             }
@@ -197,6 +213,9 @@ fun LobbyScreen(navController: NavController, model: GameModel) {
     }
 }
 
+
+
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GameScreen(navController: NavController, model: GameModel, gameId: String?) {
@@ -207,20 +226,72 @@ fun GameScreen(navController: NavController, model: GameModel, gameId: String?) 
         Scaffold(
             topBar = { TopAppBar(title =  { Text("TicTacToe - $gameId")}) }
         ) { innerPadding ->
-            Column(modifier = Modifier.padding(innerPadding)) {
+            Column(modifier = Modifier
+                .padding(innerPadding)) {
 
                 Text("Game state: ${games[gameId]!!.gameState}")
-                Text("Player 1: ${players[games[gameId]!!.player1Id]!!.name}")
-                Text("Player 2: ${players[games[gameId]!!.player2Id]!!.name}")
 
-                for (i in 0..2) {
-                    Row {
-                        for (j in 0..2) {
-                            Button(onClick = { }) {
-                                Text("Cell $i x $j")
+                Column( //Board
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(color = BabyPink)
+                        .padding(15.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Text("Game status")
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Row (
+                        horizontalArrangement = Arrangement.SpaceEvenly,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 16.dp)
+                    ){
+                        Box( modifier = Modifier
+                            .padding(5.dp)
+                            .size(100.dp)
+                            .background(Color.White, RoundedCornerShape(13.dp)),
+                            contentAlignment = Alignment.Center
+                        ){
+                            Text(text= "Player 1: ${players[games[gameId]!!.player1Id]!!.name}",
+                                style = androidx.compose.ui.text.TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold, textAlign= TextAlign.Center))
+                        }
+                        Spacer(modifier = Modifier.width(16.dp))
+
+                        Box( modifier = Modifier.padding(5.dp).size(100.dp)
+                            .background(Color.White, RoundedCornerShape(13.dp)),
+                            contentAlignment = Alignment.Center
+                        ){
+                            Text(text= "Player 2: ${players[games[gameId]!!.player2Id]!!.name}",
+                                style = androidx.compose.ui.text.TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold, textAlign= TextAlign.Center))
+                        }
+
+                    }
+                    LazyVerticalGrid(
+                        columns = GridCells.Fixed(3),
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        items(9){ index ->
+                            Box(
+                                contentAlignment = Alignment.Center,
+                                modifier = Modifier.padding(5.dp).size(100.dp).background(color = Color.White, RoundedCornerShape(13.dp))
+                                    .border(1.dp, color = Color.White, RoundedCornerShape(13.dp))
+                                    .clickable {  }
+                            ){
+                                Text(
+                                    text = "Text",
+                                    style = androidx.compose.ui.text.TextStyle(
+                                        fontSize = 83.sp,
+                                        color = BabyPink
+                                    ),
+                                    modifier = Modifier.fillMaxSize(),
+                                    textAlign = TextAlign.Center
+                                )
+
                             }
                         }
                     }
+
                 }
 
             }
